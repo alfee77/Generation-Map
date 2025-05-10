959baf5479a7c486033909b207b62bdc0abc8d15
alfee77 <103679854+alfee77@users.noreply.github.com>
2025-05-10 15:04:59 +0100

    Check box added

--- generatorMap.js
+++ generatorMap.js
@@ -1,3 +1,5 @@
+//import maplibregl from 'maplibre-gl';
+
 async function getBMUs(){
     response = await fetch(new Request('./testImportJSON.json'), {
         mode: 'no-cors'
@@ -7,5 +9,70 @@ async function getBMUs(){
     // console.log(bmus);
     return bmus;
 }
+
+
+
+const colorPalette = {
+    "Wind": "#44d444",
+    "Biomass": "#ffb399",
+    "Solar": "#ff33ff",
+    "Nuclear": "#ffff00",
+    "Natural Gas": "#00b3e6",
+    "Pumped hydro": "#2e8b57",
+    "Hydro": "#2e8b57",
+    "Coal": "#000000",
+    "Diesel/Gas Oil": "#808080",
+    "Sour Gas": "#b34d4d",
+}
+
+function addFuelLayer(sourceStuff, fillLayerStuff, lineLayerStuff, theMap) {
+    // Add the source to the map
+    theMap.addSource(sourceStuff.name, {
+        type: 'geojson',
+        data: sourceStuff.data,
+        filter: ['==', ['get', 'primaryFuel'], sourceStuff.name]
+    });
+    // Add a fill layer with some transparency
+    theMap.addLayer({
+        id: fillLayerStuff.name,
+        type: 'fill',
+        source: sourceStuff.name,
+        paint: {
+            'fill-color': colorPalette[sourceStuff.name],
+            'fill-opacity': 0.4
+        }    
+    });
+    // Add a line layer to draw the circle outline
+    // theMap.addLayer({
+    //     id: lineLayerStuff.name,
+    //     type: 'line',
+    //     source: sourceStuff.name,
+    //     paint: {
+    //         'line-color': '#0094ff',
+    //         'line-width': .5
+    //     }
+    // });
+}
+
+
+function getGenInfo(clickEvent){
+    const coordinates = clickEvent.features[0].geometry.coordinates.slice();
+    const description = `<strong> ${clickEvent.features[0].properties.name} </strong>` +
+        `<p> Installed Capacity: ${clickEvent.features[0].properties.installedCapacity} MW </p>` +
+        `<p> Primary Fuel: ${clickEvent.features[0].properties.primaryFuel} </p>`;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(clickEvent.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += click.EventlngLat.lng > coordinates[0] ? 360 : -360;
+    }
+    console.log(coordinates[0][0]);
+    new maplibregl.Popup()
+        .setLngLat(coordinates[0][0])
+        .setHTML(description)
+        .addTo(map);
+};
+
 
 
