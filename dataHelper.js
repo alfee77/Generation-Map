21e065dc197e5c5a3b2ed58b92d032207fbf9d0d
Alistair Peat <alistair.peat@gmail.com>
2025-09-12 07:58:44 +0100

    More housekeeping and code optimisation

---
+++ dataHelper.js
@@ -0,0 +1,245 @@
+let generators;
+let myChart;
+let chartArray = [];
+let chartCanvas;
+
+/**
+ * getGenerators() gets all generators from the generators.json file. It then collates all the BMUs from the json file, and formulates a string to seed the
+ * request to the Elexon API to get all Physical Notifications for each BMU, and it then embeds the PNs for that given BMU for the 24 hours up to the current
+ * settlement period
+ *
+ * @return
+ */
+
+export async function getGenerators() {
+  let response;
+  response = await fetch(new Request("./generators.json"), {
+    mode: "no-cors",
+  });
+
+  generators = await response.json();
+
+  let bmuString = "";
+
+  //This loop iterates through the generators from the JSON file and collates the bmus array for each generator.
+  //It then creates a string of BMU units to facilitate the request to the Elexon API to get all relevant Physical Notices (PNs).
+  generators.forEach((generator) => {
+    generator.bmus.forEach((bmu) => {
+      if (!bmu === "") {
+        bmuString += "&bmUnit=" + bmu;
+      }
+    });
+  });
+  //complete the BMU String
+  bmuString += "&";
+
+  //get the PNs associated with each BMU from the Elexon API
+  let allPNs = await getPNs(bmuString); //Note: PN data is stored in a data[] array within allPNs
+
+  //Now we have the latest PNs for each BMU, we can create an array of PNs against the given BMU in the generator's object.
+  generators.forEach((generator) => {
+    //This loop iterates through the generators JSON file and collates the bmus array for each generator.
+    generator.bmusObjArray = [];
+    generator.bmus.forEach((bmu) => {
+      //This inner for loop iterates through the bmus array in the generator JSON file
+      if (!bmu == "") {
+        let bmusObj = {
+          bmuId: bmu,
+          bmuPNs: [],
+        };
+
+        allPNs.forEach((PN) => {
+          //This loop iterates through the PNs and finds the relevant BMU for each generator. It then adds the relevant PNs to the generator object.
+
+          if (PN.bmUnit === bmu) {
+            bmusObj.bmuPNs.push(PN);
+          }
+        });
+        generator.bmusObjArray.push(bmusObj);
+      }
+    });
+    generator.totalOutput = 0;
+
+    //console.log(generators);
+
+    if (generator.bmusObjArray.length > 0) {
+      generator.bmusObjArray.forEach((bmuObjArray) => {
+        generator.totalOutput += bmuObjArray.bmuPNs[0].levelTo;
+      });
+    }
+  });
+  //console.log(generators);
+  return generators;
+}
+
+async function getPNs(bmusToChase) {
+  const date = new Date();
+  const dateFrom = new Date(date - 24 * 60 * 60 * 1000);
+  let offset = date.getTimezoneOffset();
+  offset *= -1;
+
+  const yyyymmdd_dateFrom =
+    dateFrom.getFullYear() +
+    "-" +
+    (dateFrom.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    dateFrom.getDate().toString().padStart(2, "0");
+
+  const yyyymmdd_dateTo =
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0");
+
+  const settlementPeriodFrom =
+    48 +
+    (date.getHours() + offset / 60) * 2 +
+    (Math.floor(date.getMinutes() / 30) + 1 - 48);
+
+  const settlementPeriodTo =
+    (date.getHours() + offset / 60) * 2 +
+    (Math.floor(date.getMinutes() / 30) + 1);
+
+  let response = await fetch(
+    new Request(
+      `https://data.elexon.co.uk/bmrs/api/v1/datasets/PN/stream?from=${yyyymmdd_dateFrom}&to=${yyyymmdd_dateTo}&settlementPeriodFrom=${settlementPeriodFrom}&settlementPeriodTo=${settlementPeriodTo}${bmusToChase}`
+    )
+  );
+
+  let PNs = await response.json();
+
+  return PNs;
+}
+
+export function getGenInfo(clickEvent, theMap) {
+  console.log(clickEvent);
+  if (chartArray.length > 0) {
+    for (let i = 0; i < chartArray.length; i++) {
+      chartArray[i].destroy();
+    }
+  }
+  const coordinates = clickEvent.features[0].geometry.coordinates.slice();
+
+  const description =
+    `<strong> ${clickEvent.features[0].properties.name} </strong>` +
+    `<p> Installed Capacity: ${clickEvent.features[0].properties.installedCapacity} MW </p>` +
+    `<p> Primary Fuel: ${clickEvent.features[0].properties.primaryFuel} </p>` +
+    `<p> Output: ${clickEvent.features[0].properties.totalOutput} MW </p>`;
+
+  // Ensure that if the map is zoomed out such that multiple
+  // copies of the feature are visible, the popup appears
+  // over the copy being pointed to.
+  while (Math.abs(clickEvent.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += click.EventlngLat.lng > coordinates[0] ? 360 : -360;
+  }
+
+  //find the entry int he generators array that matches the clicked feature
+  let ind = generators.indexOf(
+    generators.find(
+      (gen) => gen.siteName === clickEvent.features[0].properties.name
+    )
+  );
+
+  //loop through the bmusObjArray for the generator and display the chart for each BMU
+  let i;
+  for (i in generators[ind].bmusObjArray) {
+    chartCanvas = window.parent.document.getElementById(`myChart${i}`);
+    if (chartCanvas) {
+      //create data array for this bmu. Array of objects containing multiple x y values - x timeTo, y levelTo
+      let bmuChartData = [];
+      let j;
+      for (j in generators[ind].bmusObjArray[i].bmuPNs) {
+        bmuChartData.push({
+          x: generators[ind].bmusObjArray[i].bmuPNs[j].timeTo,
+          y: generators[ind].bmusObjArray[i].bmuPNs[j].levelTo,
+        });
+      }
+      displayChart(chartCanvas, ind, i, bmuChartData);
+    } else {
+      console.warn(`Chart canvas with id myChart${i} not found.`);
+    }
+  }
+
+  new maplibregl.Popup()
+    .setLngLat(coordinates[0][0])
+    .setHTML(description)
+    .addTo(theMap);
+}
+
+function displayChart(chartCanvas, genInd, bmuInd, dataPassed) {
+  window.parent.document.getElementById(
+    "chart-header"
+  ).innerHTML = `<h4>${generators[genInd].siteName} Balancing Mechanism Unit(s)</h4>`;
+
+  //setup block
+  const data = {
+    datasets: [
+      {
+        label: generators[genInd].bmusObjArray[bmuInd].bmuId,
+        data: dataPassed,
+        borderColor: colorPalette[generators[genInd].primaryFuel],
+        backgroundColor: colorPalette[generators[genInd].primaryFuel],
+        pointRadius: 1,
+        borderWidth: 1,
+        fill: false,
+        tension: 0.1,
+      },
+    ],
+  };
+
+  //config block
+  const config = {
+    type: "line",
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: `${generators[genInd].siteName}, BM Unit - ${generators[genInd].bmusObjArray[bmuInd].bmuId}`,
+        },
+      },
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "hour",
+          },
+          title: {
+            display: false,
+            text: "Time",
+          },
+        },
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: "Output (MW)",
+          },
+        },
+      },
+    },
+  };
+
+  myChart = new Chart(chartCanvas, config);
+
+  chartArray.push(myChart);
+}
+const colorPalette = {
+  Wind: "#44d444",
+  Biomass: "#ffb399",
+  Solar: "#ff33ff",
+  Nuclear: "#c91aec",
+  "Natural Gas": "#00b3e6",
+  "Pumped hydro": "#2e8b57",
+  Hydro: "#2e8b57",
+  Coal: "#000000",
+  "Diesel/Gas Oil": "#808080",
+  "Sour Gas": "#b34d4d",
+  BESS: "#00ffff",
+};
---
+++ mapDressing.js
@@ -0,0 +1,270 @@
+import { getGenerators, getGenInfo } from "./dataHelper.js";
+
+const radiusCenter = [-4.07, 55.73];
+const filterGroup = document.getElementById("filter-group");
+let generators = await getGenerators();
+
+const map = new maplibregl.Map({
+  container: "map",
+  zoom: 5,
+  center: radiusCenter,
+  style:
+    "https://api.maptiler.com/maps/dataviz/style.json?key=y8C2n98M5Gq1bNIROgJt",
+  maxZoom: 20,
+  minZoom: 5,
+  maxPitch: 85,
+});
+
+let scale = new maplibregl.ScaleControl({
+  maxWidth: 80,
+  unit: "metric", // 'imperial' or 'metric'
+});
+map.addControl(scale, "bottom-left");
+map.addControl(new maplibregl.FullscreenControl());
+
+map.on("load", async () => {
+  drawSomeStuff(map);
+}); //map.on('load', ...) function
+
+// When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+map.on("click", "Wind-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Natural Gas-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Nuclear-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Diesel/Gas Oil-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Biomass-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Coal-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Sour Gas-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Solar-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Hydro-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "Pumped hydro-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+map.on("click", "BESS-fillLayer", (e) => {
+  getGenInfo(e, map);
+});
+
+const colorPalette = {
+  Wind: "#44d444",
+  Biomass: "#ffb399",
+  Solar: "#ff33ff",
+  Nuclear: "#c91aec",
+  "Natural Gas": "#00b3e6",
+  "Pumped hydro": "#2e8b57",
+  Hydro: "#2e8b57",
+  Coal: "#000000",
+  "Diesel/Gas Oil": "#808080",
+  "Sour Gas": "#b34d4d",
+  BESS: "#00ffff",
+};
+
+function addCapacityLayer(capacityLayer, theMap) {
+  // Add the source to the map
+
+  theMap.addSource(capacityLayer.name, {
+    type: "geojson",
+    data: capacityLayer.data,
+    filter: ["==", ["get", "primaryFuel"], capacityLayer.name],
+  });
+  // Add a line layer
+  theMap.addLayer({
+    id: capacityLayer.lineName,
+    type: "line",
+    source: capacityLayer.name,
+    paint: {
+      "line-color": "#A9A9A9",
+      "line-width": 1,
+    },
+  });
+  // Add a fill layer with full transparency - this allows the click event to work
+  theMap.addLayer({
+    id: capacityLayer.fillName,
+    type: "fill",
+    source: capacityLayer.name,
+    paint: {
+      "fill-color": colorPalette[capacityLayer.name],
+      "fill-opacity": 0,
+    },
+  });
+}
+
+function addOutputLayer(outputLayer, theMap) {
+  // Add the source to the map
+
+  theMap.addSource(outputLayer.name, {
+    type: "geojson",
+    data: outputLayer.data,
+    filter: [
+      "==",
+      ["get", "primaryFuel"],
+      outputLayer.name.substring(0, outputLayer.name.length - 7),
+    ], // Remove '-output' from the name to match the primaryFuel
+  });
+
+  // Add a fill layer with some transparency
+
+  theMap.addLayer({
+    id: outputLayer.fillName + "-output",
+    type: "fill",
+    source: outputLayer.name,
+    paint: {
+      "fill-color": colorPalette[outputLayer.fillName],
+      "fill-opacity": 0.8,
+    },
+  });
+}
+
+function drawSomeStuff(theMap) {
+  let generatorLocations = `\{
+        \"type\": \"FeatureCollection\",
+        \"features\": \[`;
+
+  let generatorLocationsJSON = {};
+
+  let generatorOutputs = `\{
+        \"type\": \"FeatureCollection\",
+        \"features\": \[`;
+
+  let generatorOutputsJSON = {};
+
+  const circleOptions = {
+    steps: 200,
+    units: "meters",
+  };
+  generators.forEach((generator) => {
+    // The first part of the for loop iterates through the generators JSON file and creates a capacityCircle for each generator. It then adds the generated
+    // circle to the generatorsLocations string (this is later converted to a GeoJSON object for use in the mapping of the circles onto the map).
+    // Generate a polygon using turf.circle
+    // See https://turfjs.org/docs/#circle
+
+    let capacityCircleRadius = parseFloat(generator.installedCapacity) * 20;
+    let capacityCircle = {};
+    capacityCircle = turf.circle(
+      [parseFloat(generator.lon), parseFloat(generator.lat)],
+      capacityCircleRadius,
+      circleOptions
+    );
+    capacityCircle.properties.name = generator.siteName;
+    capacityCircle.properties.installedCapacity = generator.installedCapacity;
+    capacityCircle.properties.primaryFuel = generator.primaryFuel;
+    capacityCircle.properties.totalOutput = generator.totalOutput;
+    capacityCircle.properties.LngLatLike = [
+      parseFloat(generator.lon),
+      parseFloat(generator.lat),
+    ];
+    generatorLocations += JSON.stringify(capacityCircle) + ",";
+
+    //This next bit creates the GeoJSON string for the generator outputs. It is similar to the above, but uses the totalOutput value instead of the installedCapacity value.
+    let outputCircleRadius = 20 * generator.totalOutput;
+    let outputCircle = {};
+    outputCircle = turf.circle(
+      [parseFloat(generator.lon), parseFloat(generator.lat)],
+      outputCircleRadius,
+      circleOptions
+    );
+    outputCircle.properties.name = generator.siteName;
+    outputCircle.properties.primaryFuel = generator.primaryFuel;
+    outputCircle.properties.totalOutput = generator.totalOutput;
+    outputCircle.properties.LngLatLike = [
+      parseFloat(generator.lon),
+      parseFloat(generator.lat),
+    ];
+    generatorOutputs += JSON.stringify(outputCircle) + ",";
+  }); //for
+
+  //Complete the GeoJSON string for each circle/generation type.
+  generatorLocations =
+    generatorLocations.substring(0, generatorLocations.length - 1) + `\]}`;
+
+  generatorLocationsJSON = JSON.parse(generatorLocations);
+
+  //Complete the GeoJSON string for each generators output.
+  generatorOutputs =
+    generatorOutputs.substring(0, generatorOutputs.length - 1) + `\]}`;
+  generatorOutputsJSON = JSON.parse(generatorOutputs);
+
+  let layerArray = []; //An array to store the different generation layers.
+
+  generatorLocationsJSON.features.forEach((feature) => {
+    const layerID = feature.properties["primaryFuel"];
+
+    // Add a layer for this symbol type if it hasn't been added already.
+    if (!layerArray.includes(layerID)) {
+      layerArray.push(layerID);
+    }
+  });
+
+  /**
+   * Add the generator locations layers to the map. There is a layer for each type of generator.
+   * The data is in GeoJSON format, so we can add it as a source
+   * and then add a fill layer and an outline layer to display it.
+   **/
+
+  layerArray.forEach((layer) => {
+    let capacityLayerObject = {
+      name: `${layer}`,
+      data: generatorLocationsJSON,
+      fillName: `${layer}-fillLayer`,
+      lineName: `${layer}-lineLayer`,
+    };
+
+    addCapacityLayer(capacityLayerObject, theMap);
+
+    let outputLayerObject = {
+      name: `${layer}-output`,
+      data: generatorOutputsJSON,
+      fillName: `${layer}`,
+    };
+
+    addOutputLayer(outputLayerObject, theMap);
+
+    // Add checkbox and label elements for the layer.
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.id = layer;
+    input.checked = true;
+    filterGroup.appendChild(input);
+
+    const label = document.createElement("label");
+    label.setAttribute("for", layer);
+    label.textContent = layer;
+    filterGroup.appendChild(label);
+
+    // When the checkbox changes, update the visibility of the layer.
+    input.addEventListener("change", (e) => {
+      theMap.setLayoutProperty(
+        `${e.target.id}-lineLayer`,
+        "visibility",
+        e.target.checked ? "visible" : "none"
+      );
+      theMap.setLayoutProperty(
+        `${e.target.id}-fillLayer`,
+        "visibility",
+        e.target.checked ? "visible" : "none"
+      );
+      theMap.setLayoutProperty(
+        `${e.target.id}-output`,
+        "visibility",
+        e.target.checked ? "visible" : "none"
+      );
+    });
+  });
+}
